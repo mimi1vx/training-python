e6da3cdb50a5dbfa93133b4d9f79b565ecd53b57
Pavel Šimerda <pavlix@pavlix.net>
2016-09-21 00:26:30 +0200

    python/ii: add package organization examples

---
+++ ii/01-project-organization.examples/project/package/subpackage/module.py
@@ -0,0 +1,5 @@
+# encoding: utf-8
+
+class Example:
+    def __init__(self):
+        self.text = "Monty Python's Flying Circus"
---
+++ ii/01-project-organization.examples/project/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup(
+    name="Project",
+    version="1.0",
+    description="Example Project",
+    author="John Doe",
+    author_email="john.doe@example.net",
+    url="https://www.example.net/",
+    packages=["package"],
+    scripts=["script"],
+)
