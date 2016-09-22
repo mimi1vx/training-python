8b1aa75533e643da9c60f0590b15acaf6635717d
pavlix <pavlix@pavlix.net>
2016-09-22 09:49:11 +0200

    python/ii: add some missing examples

---
+++ ii/03-function-arguments.examples/benchmark.py
@@ -0,0 +1,34 @@
+from __future__ import print_function
+
+def benchmark(wrapped):
+    import time
+    def wrapper(*args, **kargs):
+        start = time.time()
+        result = wrapped(*args, **kargs)
+        delta = time.time() - start
+        print("function `{}(*{}, **{})` took {:.3f}".format(
+                wrapped.__name__, args, kargs, delta))
+        return result
+    return wrapper
+
+def cache(wrapped):
+    d = {}
+    def wrapper(*args, **kargs):
+        if args not in d:
+            d[args] = wrapped(*args, **kargs)
+        return d[args]
+    return wrapper
+
+@benchmark
+@cache
+def compute(a):
+    return 2**a
+
+if __name__ == '__main__':
+    x = compute(1000000)
+    x = compute(1000000)
+    x = compute(8000000)
+    x = compute(8000000)
+    x = compute(100000000)
+    x = compute(100000000)
+    x = compute(100000000)
