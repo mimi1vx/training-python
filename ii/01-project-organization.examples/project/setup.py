192fc2b25b6b2d68bcdf0122a8e605fd87346f53
Pavel Šimerda <pavlix@pavlix.net>
2016-11-25 15:25:49 +0100

    python/ii: remove author from project org

--- ii/01-project-organization.examples/project/setup.py
+++ ii/01-project-organization.examples/project/setup.py
@@ -6,8 +6,6 @@ setup(
     name="Project",
     version="1.0",
     description="Example Project",
-    author="John Doe",
-    author_email="john.doe@example.net",
     url="https://www.example.net/",
     packages=find_packages(),
     scripts=["script"],
