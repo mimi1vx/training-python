2817a23173a67ab5142985b2aa61044ac974e602
Pavel Šimerda <pavlix@pavlix.net>
2018-03-08 19:45:26 +0100

    kill python 2.x legacy code

--- i/05-control-flow.examples/exception.py
+++ i/05-control-flow.examples/exception.py
@@ -1,12 +1,5 @@
 #!/usr/bin/python
 
-from __future__ import division
-
-import sys
-
-if sys.version_info.major < 3:
-    input = raw_input
-
 try:
     x = float(input("x = "))
     y = float(input("y = "))
