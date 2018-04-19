741e6bb94717027de7c1dee2cb157b5baaadb878
Pavel Šimerda <pavlix@pavlix.net>
2018-04-19 20:48:17 +0200

    require python 3.x and make more scripts executable

--- i/05-control-flow.examples/fibonacci.py
+++ i/05-control-flow.examples/fibonacci.py
@@ -5,3 +5,5 @@ while a < 100:
     print(a)
     a, b = b, a + b
 print("Goodbye.")
+
+# 0 1 1 2 3 5 8 ...
--- i/09-inheritance.examples/shapes.py
+++ i/09-inheritance.examples/shapes.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 class Shape:
     def __init__(self):
